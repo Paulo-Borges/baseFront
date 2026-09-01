8a9fc4557aa3d827a20f434435fa25718ee01ef0
Borges <borgespaulo72@yahoo.com.br>
2026-09-01 09:54:08 -0300

    fix: atualizando contato-service

--- src/app/contato-service.ts
+++ src/app/contato-service.ts
@@ -11,14 +11,17 @@ export class ContatoService {
   private readonly apiUrl = '/api/Contato';
 
   getContatos() {
-    return this.http.get<ContatoModel[]>(this.apiUrl);
+    return this.http.get<ContatoModel[]>('/api/App');
+    // return this.http.get<ContatoModel[]>(this.apiUrl);
   }
 
   getContato(id: number) {
-    return this.http.get<ContatoModel>(`${this.apiUrl}/${id}`);
+    return this.http.get<ContatoModel>(`/api/App/${id}`);
+    // return this.http.get<ContatoModel>(`${this.apiUrl}/${id}`);
   }
 
   criarContato(contato: Omit<ContatoModel, 'id'>): Observable<ContatoModel> {
-    return this.http.post<ContatoModel>(this.apiUrl, contato);
+    return this.http.post<ContatoModel>('/api/App', contato);
+    // return this.http.post<ContatoModel>(this.apiUrl, contato);
   }
 }
