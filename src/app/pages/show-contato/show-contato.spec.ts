f4b46143ebb317f8ca6342a05669341d65174957
Borges <borgespaulo72@yahoo.com.br>
2026-09-04 09:25:52 -0300

    fix: Exluindo Arquivos .spec.ts pra iniciar Estudo de TESTES

--- src/app/pages/show-contato/show-contato.spec.ts
+++ src/app/pages/show-contato/show-contato.spec.ts
@@ -1,22 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { ShowContato } from './show-contato';
+import { of } from 'rxjs';
+import { ContatoService } from '../../services/contato-service';
 
 describe('ShowContato', () => {
   let component: ShowContato;
   let fixture: ComponentFixture<ShowContato>;
 
+  const mockContatoService = {
+    getContatos: () => of([]), // Retorna um Observable com array vazio
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ShowContato],
+      providers: [provideRouter([]), { provide: ContatoService, useValue: mockContatoService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ShowContato);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar um componente com sucesso', () => {
     expect(component).toBeTruthy();
   });
 });
